ca7890f6494337d45f966b7bdf98229fdc7fccb3
Isaac <isaacloff@gmail.com>
2026-04-24 13:59:34 +0100

    added flow to link

--- src/link/mod.rs
+++ src/link/mod.rs
@@ -1,12 +1,18 @@
+use crate::prelude::Flow;
 
 #[derive(Debug, Clone)]
 pub struct Link<Identifier> {
     pub from: Identifier,
     pub to: Identifier,
+    pub flow: Flow
 }
 
 impl<T> Link<T> {
-    pub fn new(from: T, to: T) -> Self {
-        Self { from, to }
+    pub fn free(from: T, to: T) -> Self {
+        Self { from, to, flow: Flow::Free }
+    }
+
+    pub fn new(from: T, to: T, flow: Flow) -> Self {
+        Self { from, to, flow }
     }
 }
